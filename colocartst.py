eea8d907a9186e132ad6c0781db1fc6b0fe52ddb
MrHector <hgquiros2005@gmail.com>
2024-05-16 23:27:13 -0600

    day and night
     wa wa wa

--- colocartst.py
+++ colocartst.py
@@ -1,19 +1,24 @@
 final=[]
 final.append([['+', '+', '+', '+'], ['+', '.', '.', '.'], ['.', '.', '.', '.'], ['.', '.', '.', '.']])
 final.append([['*', '.', '.', '.'], ['*', '.', '.', '.'], ['*', '*', '*', '.'], ['.', '.', '.', '.']])
+final.append([['#', '.', '.', '.'], ['#', '#', '#', '.'], ['#', '.', '.', '.'], ['.', '.', '.', '.']])
 
 piz = [["." for _ in range(6)] for _ in range(5)]
 
-def move(T,piz,i,j):
+def move(T,pizT,i,j):
+    newT=T
+    piz=cropM(pizT)
     for oi  in range(len(piz)):
         for oj in range(len(piz[0])):
-            T[oi+i][oj+j] = piz[oi][oj]
-    return T  
+            if oi+i >= len(newT) or oj+j >= len(newT[0]) or newT[oi+i][oj+j] != ".":
+                return T,False
+            newT[oi+i][oj+j] = piz[oi][oj]
+    return newT,True
 
 def cropM(M):
     Mempty = []
     Mtrans= []
-
+    
     for fila in M:
         if not all(dotn == '.' for dotn in fila):
             Mempty.append(fila)
@@ -31,6 +36,18 @@ def cropM(M):
     return res
 
 
-h=move(piz,cropM(final[0]),2,2)
-for i in h:
-    print(i)
+def call(Tab,final):
+    while final:
+        actPiz=final.pop()
+        for i in range (len(Tab)):
+            for j in range (len(Tab[0])):
+                newTab,stat=move(piz,actPiz,i,j)
+                if stat:
+                    print(final)
+                    call(newTab,final)
+    return Tab
+
+#Tab esta devolviendo una combinacion rara hasta cierto punto, final parece funcionar bien
+
+
+print(call(piz,final))
